f9a13c3354b1b220dcfcd4dbcd5f0de55c709593
#云淡然 <hi@ydr.me>
2023-03-26 01:46:24 +0800

    feat: 增强健壮性

--- src/configure.ts
+++ src/configure.ts
@@ -1,3 +1,4 @@
+import { nsLogger } from '@/utils';
 import * as core from '@actions/core';
 import * as fs from 'fs';
 import { SyncOptions, SyncTarget } from './types';
@@ -6,6 +7,7 @@ import { SyncOptions, SyncTarget } from './types';
 const DEFAULT_TIMEOUT = 30;
 
 export function parseOptions(): SyncOptions {
+  const logger = nsLogger('parseOptions');
   let name = core.getInput('name');
   // 目前仅支持 npmmirror
   const target = core.getInput('target') as SyncTarget;
@@ -13,11 +15,18 @@ export function parseOptions(): SyncOptions {
   const timeoutFinal = (Number.isNaN(timeout) ? DEFAULT_TIMEOUT : timeout) * 1000;
 
   if (!name) {
-    const data = fs.readFileSync('package.json', 'utf8');
-    name = JSON.parse(data).name;
+    try {
+      const data = fs.readFileSync('package.json', 'utf8');
+      name = JSON.parse(data).name;
+    } catch (err) {
+      logger.error('Error parsing package.json');
+      logger.error(String(err));
+      core.setFailed('package.json 读取失败');
+    }
   }
 
   if (!name) {
+    logger.error('Error loading package.json');
     core.setFailed('package.json name 获取失败');
   }
 
